531bc0188312ac957485a48632e40781a1824367
Reinis Romanovs <rromanovs@twilio.com>
2022-01-20 11:27:35 +0200

    Add prettier

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,8 +1,8 @@
-const cucumber = require('cypress-cucumber-preprocessor').default
+const cucumber = require('cypress-cucumber-preprocessor').default;
 
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  on("file:preprocessor", cucumber({typescript: require.resolve("typescript")}));
-}
+  on('file:preprocessor', cucumber({ typescript: require.resolve('typescript') }));
+};
