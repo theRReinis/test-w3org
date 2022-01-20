e4fb9f9215498f64e3896d8000348996c22aebbc
Reinis Romanovs <rromanovs@twilio.com>
2022-01-20 19:49:14 +0200

    Add more steps and step defs

--- cypress/integration/w3org.feature
+++ cypress/integration/w3org.feature
@@ -5,9 +5,12 @@ Feature: W3org
     Scenario Outline: Validate w3org page
         Given open w3org home page with <path>
         And there is no console error
-
-    Examples:
-        | path                                |
-        | 'standards/webofdevices/multimodal' |
-        | 'standards/webdesign/htmlcss'       |
-        | 'standards/badpage'                 |
+        And response code from page is <statusCode>
+        When gather all links on page
+        Then validate that all links do not respond with client error status code
+        
+        Examples:
+            | path                                 | statusCode |
+            | '/standards/webofdevices/multimodal' | 200        |
+            | '/standards/webdesign/htmlcss'       | 200        |
+            | '/standards/badpage'                 | 404        |
--- cypress/integration/w3org/w3orgStepDefs.ts
+++ cypress/integration/w3org/w3orgStepDefs.ts
@@ -1,4 +1,4 @@
-import { And, Given } from 'cypress-cucumber-preprocessor/steps';
+import { And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
 
 Given('open w3org home page with {string}', (path: string) => {
   cy.visit(path, {
@@ -11,4 +11,20 @@ Given('open w3org home page with {string}', (path: string) => {
 
 And('there is no console error', () => {
   cy.get('@consoleError').should('not.to.be.called');
+});
+
+And('response code from page is {int}', (statusCode: number) => {
+  cy.url().then((currentUrl) => {
+    cy.request({ url: currentUrl, failOnStatusCode: false }).its('status').should('eq', statusCode);
+  });
+});
+
+When('gather all links on page', () => {
+  cy.get('body').find('[href]').as('pageAnchorLinks');
+  cy.get('body').find('[src]').as('pageImageLinks');
+});
+
+Then('validate that all links do not respond with client error status code', () => {
+  cy.assertLinksStatusNotClientError('pageAnchorLinks', 'href');
+  cy.assertLinksStatusNotClientError('pageImageLinks', 'src');
 });
